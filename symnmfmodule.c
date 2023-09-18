d275e08b5ceb11635fc6a335efc24da0e699892a
frostswing <edena1@mail.tau.ac.il>
2023-09-18 16:12:55 +0300

    compiling code, still not running

--- symnmfmodule.c
+++ symnmfmodule.c
@@ -166,25 +166,23 @@ static PyObject *py_norm(PyObject *self, PyObject *args)
 
 static PyObject *py_symnmf(PyObject *self, PyObject *args)
 {
-    PyObject *py_list_H, *py_list_W, *py_list_vectors;
-    double **H, **W, *vectors;
-    int k;
+    PyObject *py_list_H, *py_list_W;
+    double **H, **W;
+    int n, k;
 
     // Parse Python arguments
-    if (!PyArg_ParseTuple(args, "OOOOi", &py_list_H, &py_list_W, &py_list_vectors, &k))
+    if (!PyArg_ParseTuple(args, "OOii", &py_list_H, &py_list_W, &n, &k))
     {
         return NULL;
     }
 
     // Convert Python list to C array for H
-    int rows_H = PyList_Size(py_list_H);
-    H = malloc(rows_H * sizeof(double *));
-    for (int i = 0; i < rows_H; i++)
+    H = malloc(n * sizeof(double *));
+    for (int i = 0; i < n; i++)
     {
         PyObject *py_row = PyList_GetItem(py_list_H, i);
-        int cols_H = PyList_Size(py_row);
-        H[i] = malloc(cols_H * sizeof(double));
-        for (int j = 0; j < cols_H; j++)
+        H[i] = malloc(n * sizeof(double));
+        for (int j = 0; j < n; j++)
         {
             PyObject *py_value = PyList_GetItem(py_row, j);
             H[i][j] = PyFloat_AsDouble(py_value);
@@ -192,61 +190,50 @@ static PyObject *py_symnmf(PyObject *self, PyObject *args)
     }
 
     // Convert Python list to C array for W
-    int rows_W = PyList_Size(py_list_W);
-    W = malloc(rows_W * sizeof(double *));
-    for (int i = 0; i < rows_W; i++)
+    W = malloc(n * sizeof(double *));
+    for (int i = 0; i < n; i++)
     {
         PyObject *py_row = PyList_GetItem(py_list_W, i);
-        int cols_W = PyList_Size(py_row);
-        W[i] = malloc(cols_W * sizeof(double));
-        for (int j = 0; j < cols_W; j++)
+        W[i] = malloc(n * sizeof(double));
+        for (int j = 0; j < n; j++)
         {
             PyObject *py_value = PyList_GetItem(py_row, j);
             W[i][j] = PyFloat_AsDouble(py_value);
         }
     }
 
-    // Convert Python list to C array for vectors
-    int len_vectors = PyList_Size(py_list_vectors);
-    vectors = malloc(len_vectors * sizeof(double));
-    for (int i = 0; i < len_vectors; i++)
-    {
-        PyObject *py_value = PyList_GetItem(py_list_vectors, i);
-        vectors[i] = PyFloat_AsDouble(py_value);
-    }
-
     // Call C function
-    ArrayInfo result = symnmf(H, W, vectors, k);
+    double **result = symnmf(H, W, n, k);
 
     // Convert C array to Python list
-    PyObject *py_result = PyList_New(result.rows);
-    for (int i = 0; i < result.rows; i++)
+    PyObject *py_result = PyList_New(n);
+    for (int i = 0; i < n; i++)
     {
-        PyObject *py_row = PyList_New(result.cols);
-        for (int j = 0; j < result.cols; j++)
+        PyObject *py_row = PyList_New(n);
+        for (int j = 0; j < n; j++)
         {
-            PyList_SetItem(py_row, j, PyFloat_FromDouble(result.array[i][j]));
+            PyList_SetItem(py_row, j, PyFloat_FromDouble(result[i][j]));
         }
         PyList_SetItem(py_result, i, py_row);
     }
 
     // Free allocated C array and result array
-    for (int i = 0; i < result.rows; i++)
+    for (int i = 0; i < n; i++)
     {
         free(H[i]);
         free(W[i]);
-        free(result.array[i]);
+        free(result[i]);
     }
     free(H);
     free(W);
-    free(vectors);
-    free(result.array);
+    free(result);
 
     return py_result;
 }
 
 // Define the methods that will be available in the Python module
-static PyMethodDef symnmfC[] = {
+// Define the Python module
+static PyMethodDef symnmfC_methods[] = {
     {"sym", py_sym, METH_VARARGS, "Calculate sym"},
     {"ddg", py_ddg, METH_VARARGS, "Calculate ddg"},
     {"norm", py_norm, METH_VARARGS, "Calculate norm"},
@@ -254,17 +241,15 @@ static PyMethodDef symnmfC[] = {
     {NULL, NULL, 0, NULL} // Sentinel
 };
 
-// Define the Python module
-static struct PyModuleDef symnmfC = {
+static struct PyModuleDef symnmfC_module = {
     PyModuleDef_HEAD_INIT,
-    "symnmfC", // Module name
-    NULL,      // Module documentation (NULL means no documentation)
-    -1,
-    symnmfC // Methods the module exposes
-};
+    "symnmfC", /* name of module */
+    NULL,      /* module documentation, may be NULL */
+    -1,        /* size of per-interpreter state of the module, or -1 if the module keeps state in global variables. */
+    symnmfC_methods};
 
 // Initialization function for the module
 PyMODINIT_FUNC PyInit_symnmfC(void)
 {
-    return PyModule_Create(&symnmfC);
+    return PyModule_Create(&symnmfC_module);
 }
